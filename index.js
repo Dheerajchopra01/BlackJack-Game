7baf106e02ee6337e803eb5e3e2e6cdf6b43cc1e
Dheerajchopra01 <Dheerajchopra01@users.noreply.github.com>
2022-09-02 03:09:33 +0530

    Refactor: Alert Message changes and its calling

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 let player = {
     name: "Guest",
-    chips: 200 
+    chips: 200
 }
 
 let betAmount = 20;
-let cards = []
-let sum = 0
-let hasBlackJack = false
-let isAlive = false
-let message = ""
-let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el")
-let cardsEl = document.getElementById("cards-el")
-let playerEl = document.getElementById("player-el")
+let cards = [];
+let sum = 0;
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
 let defaultMessage = "Want to play a round? Bet is $" + betAmount;
 
 
@@ -22,64 +24,70 @@ playerEl.textContent = player.name + ": $" + player.chips;
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
     if (randomNumber > 10) {
-        return 10
+        return 10;
     } else if (randomNumber === 1) {
-        return 11
+        return 11;
     } else {
-        return randomNumber
+        return randomNumber;
     }
 }
 
 function startGame() {
-    isAlive = true
+    isAlive = true;
     hasBlackJack = false;
-    let firstCard = getRandomCard()
-    let secondCard = getRandomCard()
-    cards = [firstCard, secondCard]
-    sum = firstCard + secondCard
-    playerEl.textContent = player.name + ": $" + player.chips;
-    renderGame()
-}
-
-function renderGame() {
-    cardsEl.textContent = "Cards: "
-    for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " "
-    }
-    
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-        player.chips += betAmount;
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-         player.chips -= betAmount;
-    }
-    messageEl.textContent = message
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     playerEl.textContent = player.name + ": $" + player.chips;
 
     if(player.chips <= 0){
-        alert("Please Reset the Game.");
+        alert("No Chips Left. Please Reset the Game.");
+        return;
+    }else{
+        renderGame();
     }
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: ";
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+    
+    sumEl.textContent = "Sum: " + sum;
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got Blackjack!";
+        hasBlackJack = true;
+        player.chips += betAmount;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+         player.chips -= betAmount;
+    }
+    messageEl.textContent = message;
+    playerEl.textContent = player.name + ": $" + player.chips; 
 }
 
 
 function newCard() {
+    if(player.chips <= 0){
+        alert("No Chips Left. Please Reset the Game.");
+        return;
+    }
     if (isAlive === true && hasBlackJack === false) {
-        let card = getRandomCard()
-        sum += card
-        cards.push(card)
-        renderGame()        
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
     }
 }
 
 function reset(){
-     hasBlackJack = false
-     isAlive = false
+     hasBlackJack = false;
+     isAlive = false;
 
     cards=[];
     cardsEl.textContent ="Cards:";
